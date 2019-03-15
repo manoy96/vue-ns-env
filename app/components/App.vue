a835301703c65cfbbeea9e1ac96b2f369219f8a8
manoy96 <mfewcar@gmail.com>
2019-03-14 22:57:24 -0600

    working on tab 1 layout flexbox

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,18 +1,27 @@
 <template>
     <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/>
+        <!-- <ActionBar title="Welcome to NativeScript-Vue!" android:flat="true"/> -->
         <TabView android:tabBackgroundColor="#53ba82"
                  android:tabTextColor="#c4ffdf"
                  android:selectedTabTextColor="#ffffff"
                  androidSelectedTabHighlightColor="#ffffff" >
 
             <TabViewItem title="Tab 1">
-                <GridLayout columns="*,*,*" rows="*,*,*,*,*,*,*,*">
-                    <!-- <Label class="message" :text="msg" col="1" row="0"/> -->
-                    <Label text="Where To?" class="message" col="1" row="0"/>
-                    <TextField class="myInput" :text="textFieldValue" hint="Current Location" col="1" row="1" />
-                    <TextField :text="textFieldValue" hint="Destination" col="1" row="2" />
-                </GridLayout>
+                <!-- <GridLayout columns="*,*,*" rows="*,*,*,*,*,*,*,*">
+                    <Label class="h1" text="Where To?" col="1" row="0"/>
+                    <TextField class="m-x-auto" :text="textFieldValue" hint="Current Location" col="1" row="1" />
+                    <TextField class="m-x-auto" :text="textFieldValue" hint="Destination" col="1" row="2" />
+                </GridLayout> -->
+                <FlexboxLayout flexDirection="column" 
+                                alignItems="center"
+                                justifyConten="space-between" >
+                                
+                    <Label class="h1 m-y-15" text="Where To?" col="1" row="0"/>
+                    <Label class="h2 p-l-20 m-b-30 " alignSelf="flex-start" text="Current Location" col="1" row="0"/>
+                    <TextField class="p-y-5 p-x-15 myInput" :text="textFieldValue" hint="Current Location" col="1" row="1" />
+                    <TextField class="p-y-5 p-x-15 myInput" :text="textFieldValue" hint="Destination" col="1" row="2" />
+
+                </FlexboxLayout>
             </TabViewItem>
 
             <TabViewItem title="Tab 2">
@@ -26,6 +35,7 @@
                     <Label class="message" text="Tab 3 Content" col="0" row="0"/>
                 </GridLayout>
             </TabViewItem>
+
         </TabView>
     </Page>
 </template>
@@ -52,8 +62,8 @@
         font-size: 20;
         color: #333333;
     }
-    .myinput {
+    .myInput {
         margin: 0 auto;
-        border-bottom: solid 2px #2e3140;
+        border: solid 2px #2e3140;
     }
 </style>
