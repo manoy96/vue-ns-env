df10f78c95731848faf1a9cd561324810c47a6fd
manoy96 <mfewcar@gmail.com>
2019-03-14 18:31:06 -0600

    basic edits to tab 1

--- app/components/App.vue
+++ app/components/App.vue
@@ -5,16 +5,22 @@
                  android:tabTextColor="#c4ffdf"
                  android:selectedTabTextColor="#ffffff"
                  androidSelectedTabHighlightColor="#ffffff" >
+
             <TabViewItem title="Tab 1">
-                <GridLayout columns="*" rows="*">
-                    <Label class="message" :text="msg" col="0" row="0"/>
+                <GridLayout columns="*,*,*" rows="*,*,*,*,*,*,*,*">
+                    <!-- <Label class="message" :text="msg" col="1" row="0"/> -->
+                    <Label text="Where To?" class="message" col="1" row="0"/>
+                    <TextField class="myInput" :text="textFieldValue" hint="Current Location" col="1" row="1" />
+                    <TextField :text="textFieldValue" hint="Destination" col="1" row="2" />
                 </GridLayout>
             </TabViewItem>
+
             <TabViewItem title="Tab 2">
                 <GridLayout columns="*" rows="*">
                     <Label class="message" text="Tab 2 Content" col="0" row="0"/>
                 </GridLayout>
             </TabViewItem>
+
             <TabViewItem title="Tab 3">
                 <GridLayout columns="*" rows="*">
                     <Label class="message" text="Tab 3 Content" col="0" row="0"/>
@@ -28,7 +34,7 @@
   export default {
     data() {
       return {
-        msg: 'Hello World!'
+        msg: 'Where To?'
       }
     }
   }
@@ -36,7 +42,7 @@
 
 <style scoped>
     ActionBar {
-        background-color: #53ba82;
+        background-color: #c11327;
         color: #ffffff;
     }
 
@@ -45,5 +51,9 @@
         text-align: center;
         font-size: 20;
         color: #333333;
+    }
+    .myinput {
+        margin: 0 auto;
+        border-bottom: solid 2px #2e3140;
     }
 </style>
